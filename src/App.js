c58eda84c2b1bab75905bc4f9d7b24496922c185
Daniel Hernandez <edgardaniel1023@gmail.com>
2020-07-30 21:41:17 -0500

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Listado from './components/Listado';
 
 function App() {
+
+  //* definir categoria y nticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url =`http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=b910aeec0d0342bc97c08bb4e76657bd`
+      const respuesta = await fetch (url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarApi();
+  }, [categoria]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Fragment>
+     <Header titulo="Noticias Bajio"/>
+     
+     <div className="container white">
+      <Formulario 
+      guardarCategoria={guardarCategoria}/>
+     </div>
+     <Listado 
+     noticias={noticias}/>
+   </Fragment>
   );
 }
 
---
+++ src/components/Listado.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Noticia from './Noticia';
+import PropTypes from 'prop-types';
+
+const Listado = ({noticias}) => (
+        <div className="row">
+            {noticias.map(noticia => (
+                <Noticia 
+                key={noticia.url}
+                noticia={noticia}/>
+            ))}
+        </div>
+
+);
+
+Listado.propTypes = {
+    noticias: PropTypes.array.isRequired
+}
+ 
+export default Listado;
